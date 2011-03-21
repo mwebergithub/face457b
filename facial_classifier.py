9422a1ae1fe4da426c302952b907b1ceb8cd1f18
Artem Avdacev <artem.avdacev@gmail.com>
2011-03-21 12:30:39 -0400

    shitty train routine with smoke test for the Kohonen method

--- facial_classifier.py
+++ facial_classifier.py
@@ -10,8 +10,12 @@ from pybrain.structure           import FeedForwardNetwork
 from pybrain.structure           import LinearLayer, SigmoidLayer
 from pybrain.structure           import FullConnection
 
+from scipy.ndimage import maximum_position
+from scipy import zeros, array 
+from custom_kohonen              import KohonenMap
+
 # Most obviously tunable params
-EPOCHS = 100
+EPOCHS = 200
 IMG_SIZE = 400
 
 # Emotion "enums"
@@ -208,10 +212,80 @@ class SupervisedFacialClassifier:
 
 
 class UnsupervisedFacialClassifier:
+  som = None
+  emo_clusters = {}
+
+  def __init__(self, nNeurons, mNeurons):
+    # KohonenMap of IMG_SIZE dimensions and a 2x2 grid. (try 4x1 later)
+    self.som = KohonenMap(IMG_SIZE, nNeurons, mNeurons);
+
+    emo_clusters = {
+      Emotion.SAD: [],
+      Emotion.SMILING: [],
+      Emotion.CALM: [],
+      Emotion.ASTONISHED: []
+    }
+    
+
   def train(self, inputData, verbose=True):
+    # Sort the known emotions into image piles, to assosciate a cluster with an
+    # emotion after training, store that assosciation in emo_cluster
+    cluster_identification_images = {
+      Emotion.SAD: [],
+      Emotion.SMILING: [],
+      Emotion.CALM: [],
+      Emotion.ASTONISHED: []
+    }
+
+    train_faces = []
+    test_faces = []
+    for entry in inputData:
+      if (random() > 0.25):
+        # Build training set with 75% of inputData
+        (emotion, img) = entry
+        cluster_identification_images[emotion].append(img)
+        train_faces.append(img)
+      else:
+        # Build test set with the other 25%
+        test_faces.append(entry)
+
+    i = 0
+    # Should the number of epochs be consistent with supervised?
+    while self.som.neighbours > 0.5:
+    #for epoch in range(EPOCHS):
+      for img in train_faces:
+        # Not sure if this can be done with som.activateOnDataset()
+        self.som.activate(img)
+        self.som.backward()
+
+
+      i += 1
+      if not (i % 10):
+        print "SOM neighbors: %f" % self.som.neighbours
+
+
+    for emotion in Emotion.to_s.keys():
+      emo_count = zeros((self.som.nNeurons, self.som.mNeurons))
+      for img in cluster_identification_images[emotion]:
+        self.som.activate(img)
+        emo_count[self.som.winner[0]][self.som.winner[1]] += 1
+      dominant_node = maximum_position(emo_count)
+      self.emo_clusters[emotion] = dominant_node
+      
+      print emo_count
+      print len(cluster_identification_images[emotion])
+      print "Emo-Training Error: %f" % (1.0 - (emo_count[dominant_node[0]][dominant_node[1]] / 
+                                    len(cluster_identification_images[emotion])))
+
+    print "Final emo-map:"
+    for emotion in Emotion.to_s.keys():
+      print "%s: %s" % (Emotion.to_s[emotion], self.emo_clusters[emotion])
+
+
+
     return False
 
-  def classify(self, facialData ):
+  def classify(self, facialData, verbose=True ):
     return False
 
 
@@ -228,8 +302,17 @@ class RandomFaceGen:
   @classmethod
   def genRandomInputSet(cls):
     inputData = []
-    for i in range(10):
-      emo = int(random()*3)
+    for i in range(20):
+      emo = int(random()*4)
       inputData.append((emo, tuple(cls.genRandomImg())))
 
     return inputData
+
+#################### TEST
+
+f = UnsupervisedFacialClassifier(2,2)
+t = RandomFaceGen.genRandomInputSet()
+f.train(t)
+for i in xrange(10):
+  img = RandomFaceGen.genRandomImg()
+  f.classify(img)
